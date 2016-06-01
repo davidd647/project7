b267ba312fb6aca2680bb38272d44d774057cbeb
David Dales <david.c.dales@gmail.com>
2016-06-01 15:42:26 -0400

    2016-06-01

--- page.php
+++ page.php
@@ -4,13 +4,40 @@
   <div class="container">
 
     <div class="content">
-      <?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        <?php 
+          if( have_posts() ) {
+            while (have_posts()){
+              // the_post();
+              // the_title();
+              ?>
+              <article class="blog-post">
+                <h3><?php the_title(); ?></h3>
+                
+                <p>
+                  Posted on 
+                  <a href="<?php echo get_day_link(); ?>">
+                    <?php the_time('F j, Y g:i a'); ?> 
+                  </a>
+                  by 
+                  <a href="<?php 
+                    $author_id = get_the_author_meta('ID');
+                    $author_nickname = get_the_author_meta('user_nicename');
+                    echo get_author_posts_url($author_id, $author_nickname); 
+                  ?>">
+                    <?php the_author(); ?>
+                  </a>
+                </p>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-
-      <?php endwhile; // end the loop?>
+              <?php 
+                echo get_avatar("david.c.dales@gmail.com", 96);
+              ?>
+              <?php the_excerpt(); ?>
+                
+              </article>
+              <?php
+              }
+            }
+        ?>
     </div> <!-- /,content -->
 
     <?php get_sidebar(); ?>
